e4dc3c04799f9cb479c26a50ead48d48715515f7
Zara Martín <zmartand@myuax.es>
2023-03-06 21:53:47 +0100

    Main

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -2,6 +2,8 @@ package org.example;
 
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        Diccionario diccionario = new Diccionario();
+        diccionario.leerArchivo("palabras.csv");
+        diccionario.imprimirTablaHash();
     }
 }
